42295adeb79506e9af903a1bb978244b74459962
Allen Yin <yinhaonan55@gmail.com>
2017-01-12 19:58:06 -0800

    hello

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
-    historyApiFallback: true
+      headers: { "Access-Control-Allow-Origin": "*" },
+      historyApiFallback: true
   }
 };
